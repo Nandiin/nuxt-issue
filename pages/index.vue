3a7e6b629f7c8cac1024f16a5498d3b914f6c812
Nandiin <nandiin@163.com>
2019-01-29 18:56:19 +0800

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,67 @@
+<template>
+<div>
+  <div class="app">
+    <h1>Bank app</h1>
+    <div>Fake data: {{ overview }}</div>
+    <div>(should be loaded once and persist when route changes)</div>
+    <div class="tabbar">
+      <nuxt-link to="/1" class="tab">Card 1</nuxt-link>
+      <nuxt-link to="/2" class="tab">Card 2</nuxt-link>
+    </div>
+    <nuxt-child class="tabpane"/>
+  </div>
+  <div class="log" ref="log">
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return { overview: Math.random() }
+  },
+  watch: {
+    overview (newVal, oldVal) {
+      const p = document.createElement('p')
+      p.innerText = `data.overview changed from ${oldVal} to ${newVal}`
+      this.$refs.log.appendChild(p)
+    }
+  }
+}
+</script>
+
+<style>
+a {
+  text-decoration: none;
+}
+</style>
+
+<style scoped>
+.app {
+  text-align: center;
+  line-height: 1.5;
+  width: 480px;
+  margin: auto;
+  background: lightgray;
+}
+.nuxt-link-active {
+  background: gray;
+}
+.tabbar {
+  display: flex;
+}
+.tab {
+  width: 50%;
+  height: 40px;
+  line-height: 40px;
+}
+.tabpane {
+  background: gray;
+  height: 500px;
+  color: white;
+}
+.log {
+  text-align: center;
+}
+</style>
---
+++ pages/index/_type/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>
+      <nuxt-link to="?range=week" :class="{ active: range === 'week' }" replace>7 Days</nuxt-link> |
+      <nuxt-link to="?range=month" :class="{ active: range === 'month' }" replace>1 Month</nuxt-link> |
+      <nuxt-link to="?range=all" :class="{ active: range === 'all' }" replace>All</nuxt-link>
+      <div>
+        <p v-for="i in 10" :key="i" style="text-align: left; padding: 0 10px">2019.01.01<span style="float: right">${{ (i % 2 ? 1 : -1) * i * 100 }}.00</span></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    range () {
+      return ['month', 'all'].includes(this.$route.query.range) ? this.$route.query.range : 'week'
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.active {
+  color: yellowgreen;
+}
+
+</style>
